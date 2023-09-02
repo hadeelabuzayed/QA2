a7c1f16e147009c0b80ec8c91197d1f57e64975b
Hadeel Abuzayed <hadeelabuzayed01@gmail.com>
2023-09-02 12:31:55 +0300

    checkout is done

--- cypress/e2e/finaltest.cy.js
+++ cypress/e2e/finaltest.cy.js
@@ -1,41 +1,38 @@
-/// <reference types="Cypress" />
-
-describe('Sign up & login test', () => {
-
-  let userName = 'abuzayed';
-  let password = 'swe123**';
-  beforeEach(() => {
-    cy.visit("https://automationteststore.com/index.php?rt=account/login");
-  });
-  it('sign up', () => {
-    // cy.visit("https://automationteststore.com/index.php?rt=account/login")
-    cy.get('#accountFrm > fieldset > .btn').click();
-    cy.get('#AccountFrm_firstname').type("hadeel");
-    cy.get('#AccountFrm_lastname').type("abuzayed");
-    cy.get('#AccountFrm_email').type("hadeelabuzayed8@gmail.com");
-    cy.get('#AccountFrm_telephone').type("0788670533");
-    cy.get('#AccountFrm_fax').type("1515");
-    cy.get('#AccountFrm_company').type("ltuc");
-    cy.get('#AccountFrm_address_1').type("Jabal Tareq");
-    cy.get('#AccountFrm_address_2').type("Jabal Tareq");
-    cy.get('#AccountFrm_country_id').select("Jordan");
-    cy.get('#AccountFrm_city').type("Zarqa");
-    // cy.get('#AccountFrm_zone_id').select("Jordan");
-    cy.get('#AccountFrm_postcode').type("5010");
-
-    cy.get('#AccountFrm_loginname').type(userName);
-    cy.get('#AccountFrm_password').type(password);
-    cy.get('#AccountFrm_confirm').type(password);
-    cy.get('#AccountFrm_newsletter1').first().check();
-    cy.get('#AccountFrm_agree').check();
-    cy.get('.col-md-2 > .btn').click();
-  });
 
 
-  it('login demo', () => {
-    cy.get('#loginFrm_loginname').type(userName);
+
+///reference types ="cypress"/>
+beforeEach(() => {
+  cy.visit("https://automationteststore.com/");
+
+});
+describe('Checkout process', () => {
+  let name = "abuzayed";
+  let password = "swe123**";
+
+
+
+  it('add items to the cart', () => {
+
+    cy.get('#customer_menu_top > li >a').click();
+    cy.get('#loginFrm_loginname').type(name);
     cy.get('#loginFrm_password').type(password);
     cy.get('#loginFrm > fieldset > .btn > .fa').click();
+    cy.get('.logo > img').click();
+
+
+    cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .thumbnail > :nth-child(1) > img').click()
+    cy.get('.cart').click();
+    cy.get('#cart_checkout1').click();
+    cy.get('#checkout_btn').click();
+    cy.get('.maintext').should('include.text', 'Your Order Has Been Processed!');
+
+
+
+
+
+
+
   });
 
 });
