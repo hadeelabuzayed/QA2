bf76950e66aa28511910ebec63531999979e8766
Hadeel Abuzayed <hadeelabuzayed01@gmail.com>
2023-09-02 10:28:25 +0300

    checkout

--- cypress/e2e/finaltest.cy.js
+++ cypress/e2e/finaltest.cy.js
@@ -1,81 +1,41 @@
-///reference types ="cypress"/>
+/// <reference types="Cypress" />
 
-beforeEach(() => {
-  cy.visit("https://www.automationteststore.com/");
-});
+describe('Sign up & login test', () => {
 
-describe('Print the name of the first item and the last item ', () => {
-  it('first category - FEATURED', () => {
+  let userName = 'abuzayed';
+  let password = 'swe123**';
+  beforeEach(() => {
+    cy.visit("https://automationteststore.com/index.php?rt=account/login");
+  });
+  it('sign up', () => {
+    // cy.visit("https://automationteststore.com/index.php?rt=account/login")
+    cy.get('#accountFrm > fieldset > .btn').click();
+    cy.get('#AccountFrm_firstname').type("hadeel");
+    cy.get('#AccountFrm_lastname').type("abuzayed");
+    cy.get('#AccountFrm_email').type("hadeelabuzayed8@gmail.com");
+    cy.get('#AccountFrm_telephone').type("0788670533");
+    cy.get('#AccountFrm_fax').type("1515");
+    cy.get('#AccountFrm_company').type("ltuc");
+    cy.get('#AccountFrm_address_1').type("Jabal Tareq");
+    cy.get('#AccountFrm_address_2').type("Jabal Tareq");
+    cy.get('#AccountFrm_country_id').select("Jordan");
+    cy.get('#AccountFrm_city').type("Zarqa");
+    // cy.get('#AccountFrm_zone_id').select("Jordan");
+    cy.get('#AccountFrm_postcode').type("5010");
 
-
-    cy.get('#featured > .container-fluid').find('.prdocutname').first().invoke('text').then((the_text) => {
-      cy.log("first item inside the Featured Category is : ", the_text)
-    })
-
-    cy.get('#featured > .container-fluid').find('.prdocutname').last().invoke('text').then((the_text) => {
-      cy.log("last item inside the Featured Category is : ", the_text)
-
-    })
+    cy.get('#AccountFrm_loginname').type(userName);
+    cy.get('#AccountFrm_password').type(password);
+    cy.get('#AccountFrm_confirm').type(password);
+    cy.get('#AccountFrm_newsletter1').first().check();
+    cy.get('#AccountFrm_agree').check();
+    cy.get('.col-md-2 > .btn').click();
   });
 
-  it('second category - latest products', () => {
 
-
-    cy.get('#latest > .container-fluid').find('.prdocutname').first().invoke('text').then((the_text) => {
-      cy.log("first item inside the latest Category is : ", the_text)
-    })
-
-    cy.get('#latest > .container-fluid').find('.prdocutname').last().invoke('text').then((the_text) => {
-      cy.log("last item inside the latest Category is : ", the_text)
-
-    })
+  it('login demo', () => {
+    cy.get('#loginFrm_loginname').type(userName);
+    cy.get('#loginFrm_password').type(password);
+    cy.get('#loginFrm > fieldset > .btn > .fa').click();
   });
-});
 
-describe("this is to see the price multiply by 10 ", () => {
-  it("hey we need to see the price multiply by 10 for the first item inside best seller category", () => {
-
-    cy.get("#bestseller > .container-fluid")
-      .find(".oneprice")
-      .first()
-      .invoke("text")
-      .then((the_text) => {
-
-        let ModifiedPrice = (the_text.replace("$", "")) * 10
-        cy.log(ModifiedPrice)
-        let myage = 30
-
-        myage += myage
-
-        cy.log(myage)
-      });
-  });
-});
-
-
-
-
-
-describe('summation of special category', () => {
-  it('the sum of all old and all new prices in the SPECIALS category', () => {
-    let SumOldPrices = 0;
-    let SumNewPrices = 0;
-    cy.get('#special > .container-fluid')
-      .find('.pricenew')
-      .each((new_price_text, index) => {
-        cy.get('#special > .container-fluid')
-          .find('.priceold')
-          .eq(index)
-          .invoke('text')
-          .then(old_price_text => {
-            const OldPrice = parseInt(old_price_text.replace('$', ''));
-            const NewPrice = parseInt(new_price_text.text().replace('$', ''));
-
-            SumOldPrices += OldPrice;
-            SumNewPrices += NewPrice;
-          });
-      }).then(() => {
-        alert(` the sum of the old prices is ${SumOldPrices} and the sum of the new prices is ${SumNewPrices}`)
-      });
-  });
 });
